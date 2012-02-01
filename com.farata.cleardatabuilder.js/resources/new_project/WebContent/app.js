4a7e35b353e0a194db3aa6f847271b022f1d61ae
alex_yuzafovich <alex_yuzafovich@9db0787f-aee9-4a4d-a695-80f0cfb65743>
2012-02-01 14:06:10 +0000

    git-svn-id: https://cleartoolkit.svn.sourceforge.net/svnroot/cleartoolkit/cdbjs/trunk@938 9db0787f-aee9-4a4d-a695-80f0cfb65743

--- com.farata.cleardatabuilder.js/resources/new_project/WebContent/app.js
+++ com.farata.cleardatabuilder.js/resources/new_project/WebContent/app.js
@@ -24,7 +24,14 @@ Ext.require(
               
             ],
             launch: function() {
-            	
+            	Ext.create('Ext.container.Viewport', {
+                    layout: 'fit',
+                    items: [
+                        {
+                            xtype: ''
+                        }
+                    ]
+                });
             }
 		});		
 	}
