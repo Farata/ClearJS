58f4224d38ed46351c74495ab1ebcacab836faca
alex_yuzafovich <alex_yuzafovich@9db0787f-aee9-4a4d-a695-80f0cfb65743>
2012-02-11 08:56:31 +0000

    CDB JS
    git-svn-id: https://cleartoolkit.svn.sourceforge.net/svnroot/cleartoolkit/cdbjs/trunk@1031 9db0787f-aee9-4a4d-a695-80f0cfb65743

--- com.farata.cleardatabuilder.js/resources/new_project/WebContent/app/model/AssociateDTO.js
+++
@@ -1,5 +0,0 @@
-Ext.define('CDB.model.AssociateDTO',{
-	extend:'Ext.data.Model',
-	fields:['id','companyId','uid','associate'],
-	belongsTo: 'CDB.model.CompanyDTO'
-});
--- com.farata.cleardatabuilder.js/resources/new_project/WebContent/app/model/CompanyDTO.js
+++
@@ -1,5 +0,0 @@
-Ext.define('CDB.model.CompanyDTO',{
-	extend:'Ext.data.Model',
-	fields:['id','company'],
-	hasMany:'CDB.model.AssociateDTO'
-});
