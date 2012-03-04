086615366d85ef06ed0286b3615bb600a96972dc
alex_yuzafovich <alex_yuzafovich@9db0787f-aee9-4a4d-a695-80f0cfb65743>
2012-03-04 11:48:58 +0000

    CDBJS
    git-svn-id: https://cleartoolkit.svn.sourceforge.net/svnroot/cleartoolkit/HTML5/trunk@1212 9db0787f-aee9-4a4d-a695-80f0cfb65743

---
+++ com.farata.cleardatabuilder.js/src/com/farata/cleardatabuilder/js/jpa/platform/CDBJpaPlatformFactory.java
@@ -0,0 +1,8 @@
+package com.farata.cleardatabuilder.js.jpa.platform;
+
+import org.eclipse.jpt.jpa.core.internal.GenericJpaPlatformFactory;
+
+@SuppressWarnings("restriction")
+public class CDBJpaPlatformFactory extends GenericJpaPlatformFactory {
+	
+}
