48400d8eee299f1155c792a10335eccdcd551619
vrasputnis <vrasputnis@9db0787f-aee9-4a4d-a695-80f0cfb65743>
2012-03-15 18:40:26 +0000

    git-svn-id: https://cleartoolkit.svn.sourceforge.net/svnroot/cleartoolkit/HTML5/trunk@1261 9db0787f-aee9-4a4d-a695-80f0cfb65743

---
+++ com.farata.cleardatabuilder.js/resources/sample_project/WebContent/app.js
@@ -0,0 +1,25 @@
+Ext.Loader.setConfig({
+    disableCaching: false,
+    enabled: true,
+    paths: {
+        Clear: 'clear',
+        **{app.name}**: 'app'
+    }
+});
+
+Ext.syncRequire('**{app.name}**.init.InitDirect');
+
+// Launch the application 
+Ext.application({
+    name: '**{app.name}**',
+    requires: [
+        'Clear.override.ExtJSOverrider',
+        '**{app.name}**.override.ExtJSOverrider'
+    ],
+    controllers: [
+      'ToolbarController',
+      'CompanyListController',
+      'AssociateController'
+    ],
+    autoCreateViewport: true
+});
