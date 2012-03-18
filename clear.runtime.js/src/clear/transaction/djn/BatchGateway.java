934eda98abfc137d13a12b3110e9034e734092cd
alex_yuzafovich <alex_yuzafovich@9db0787f-aee9-4a4d-a695-80f0cfb65743>
2012-03-18 15:16:37 +0000

    CDBJS
    git-svn-id: https://cleartoolkit.svn.sourceforge.net/svnroot/cleartoolkit/HTML5/trunk@1269 9db0787f-aee9-4a4d-a695-80f0cfb65743

--- clear.runtime.js/src/clear/transaction/djn/BatchGateway.java
+++ clear.runtime.js/src/clear/transaction/djn/BatchGateway.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import java.util.List;
 
-
 import com.google.gson.Gson;
 import com.softwarementors.extjs.djn.api.RegisteredAction;
 import com.softwarementors.extjs.djn.api.RegisteredStandardMethod;
@@ -14,43 +13,63 @@ import com.softwarementors.extjs.djn.api.Registry;
 import com.softwarementors.extjs.djn.config.annotations.DirectMethod;
 import clear.djn.router.RequestRouter;
 import clear.transaction.BatchMember;
+import clear.transaction.UserTransactionManager;
 
 import com.softwarementors.extjs.djn.router.dispatcher.Dispatcher;
 import com.softwarementors.extjs.djn.router.processor.RequestException;
 
-
 public class BatchGateway {
-
 
 	@DirectMethod
 	public List<BatchMember> execute(List<BatchMember> items) {
+
+		try {
+			UserTransactionManager.joinUserTransaction();
+			List<BatchMember> res = executeInternal(items);
+			UserTransactionManager.commitUserTransaction();
+			return res;
+		} catch (Throwable e) {
+			try {
+				UserTransactionManager.rollbackUserTransaction();
+			} catch (Throwable th) {
+				throw new RuntimeException(th);
+			}
+			throw new RuntimeException(e);
+		}
+	}
+
+	private List<BatchMember> executeInternal(List<BatchMember> items) {
 
 		RequestRouter router = RequestRouter.getRequestRouter();
 		Registry registry = router.getRegistry();
 		Dispatcher dispatcher = router.getDispatcher();
 		Gson gson = new Gson();
-		
+
 		List<BatchMember> results = new ArrayList<BatchMember>(items.size());
-		
+
 		for (int i = 0; i < items.size(); i++) {
 			@SuppressWarnings("unchecked")
-			Map<String, String> mapItem = (Map<String, String>)items.get(i);
+			Map<String, String> mapItem = (Map<String, String>) items.get(i);
 			String jsonItem = gson.toJson(mapItem);
-			BatchMember batchMember = gson.fromJson(jsonItem, BatchMember.class);
-			RegisteredAction action = registry.getAction(batchMember.className); 
-			if( action == null ) {
-			      throw RequestException.forActionNotFound( batchMember.className );
+			BatchMember batchMember = gson
+					.fromJson(jsonItem, BatchMember.class);
+			RegisteredAction action = registry.getAction(batchMember.className);
+			if (action == null) {
+				throw RequestException.forActionNotFound(batchMember.className);
 			}
 
-			RegisteredStandardMethod method = action.getStandardMethod(batchMember.methodName);
-			if( method == null ) {
-				throw RequestException.forActionMethodNotFound( action.getName(), batchMember.methodName );
+			RegisteredStandardMethod method = action
+					.getStandardMethod(batchMember.methodName);
+			if (method == null) {
+				throw RequestException.forActionMethodNotFound(
+						action.getName(), batchMember.methodName);
 			}
-		
-			batchMember.result = dispatcher.dispatch(method, batchMember.parameters.toArray());
+
+			batchMember.result = dispatcher.dispatch(method,
+					batchMember.parameters.toArray());
 			results.add(batchMember);
-		}	
-		
+		}
+
 		return results;
 	}
 }
