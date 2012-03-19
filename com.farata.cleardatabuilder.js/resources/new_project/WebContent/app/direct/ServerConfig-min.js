398e2c1e8949b8e5c41781060b2892d53147c327
vrasputnis <vrasputnis@9db0787f-aee9-4a4d-a695-80f0cfb65743>
2012-03-19 18:03:30 +0000

    git-svn-id: https://cleartoolkit.svn.sourceforge.net/svnroot/cleartoolkit/HTML5/trunk@1276 9db0787f-aee9-4a4d-a695-80f0cfb65743

--- com.farata.cleardatabuilder.js/resources/new_project/WebContent/app/direct/ServerConfig-min.js
+++
@@ -1,5 +0,0 @@
-Ext.namespace("**{app.name}**.direct.config");
-Ext.namespace("**{app.name}**.direct.action");
-**{app.name}**.direct.config.PROVIDER_BASE_URL=window.location.protocol+"//"+window.location.host+"/"+(window.location.pathname.split("/").length>2?window.location.pathname.split("/")[1]+"/":"")+"djn/directprovider";
-**{app.name}**.direct.config.POLLING_URLS={};
-**{app.name}**.direct.config.REMOTING_API={url:**{app.name}**.direct.config.PROVIDER_BASE_URL,type:"remoting",namespace:**{app.name}**.direct.action,actions:{CompanyAction:[{name:"getCompanies_insertItems",len:1,formHandler:false},{name:"getCompanies_deleteItems",len:1,formHandler:false},{name:"getCompanies_updateItems",len:1,formHandler:false},{name:"getCompanies",len:0,formHandler:false}],CompanyAssociateAction:[{name:"getCompanyAssociates_deleteItems",len:1,formHandler:false},{name:"getCompanyAssociates_updateItems",len:1,formHandler:false},{name:"getCompanyAssociates",len:1,formHandler:false},{name:"getCompanyAssociates_insertItems",len:1,formHandler:false}]}};
