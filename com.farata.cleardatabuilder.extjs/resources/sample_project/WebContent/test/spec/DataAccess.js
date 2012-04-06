e04bc0e217a32f8011f15fd24be72b8fd2f31d06
vrasputnis <vrasputnis@9db0787f-aee9-4a4d-a695-80f0cfb65743>
2012-04-06 16:15:18 +0000

    git-svn-id: https://cleartoolkit.svn.sourceforge.net/svnroot/cleartoolkit/HTML5/trunk@1487 9db0787f-aee9-4a4d-a695-80f0cfb65743

--- com.farata.cleardatabuilder.extjs/resources/sample_project/WebContent/test/spec/DataAccess.js
+++ com.farata.cleardatabuilder.extjs/resources/sample_project/WebContent/test/spec/DataAccess.js
@@ -1,120 +1,175 @@
 Ext.define('Test.spec.DataAccess' ,{}, function () {
 	
-	var store = null, toolbarCtrl = null, companyListCtrl = null, companyCount, company;
+	var companyStore = null, toolbarCtrl = null, companyCtrl = null;
  	
 	describe("Companies", function() {
         var modifiedCompanyName;
 
  	    beforeEach(function(){
  	        if (!toolbarCtrl) {
- 	        	toolbarCtrl = Application.getController('ToolbarController');
+ 	        		toolbarCtrl = Application.getController('ToolbarController');
  	        }
 
 	        expect(toolbarCtrl).toBeTruthy();
 	        
- 	        if (!companyListCtrl) {
- 	        	companyCtrl = Application.getController('CompanyController');
+ 	        if (!companyCtrl) {
+ 	        		companyCtrl = Application.getController('CompanyController');
  	        }
  	        
 	        expect(companyCtrl).toBeTruthy();
 
- 	        if (!store) {
- 	        	store = toolbarCtrl.getExampleCompanyStore();
+	        if (!companyStore) {
+	        		companyStore = toolbarCtrl.getExampleCompanyStore();
  	        }
 
- 	        expect(store).toBeTruthy();
+ 	        expect(companyStore).toBeTruthy();
  	        
  	    });
 
  	    it("should have companies",function(){
+
  	        toolbarCtrl.fill();
 
  	        waitsFor(
- 	        	function(){ return !store.isLoading(); },
+ 	        	function(){ return !companyStore.isLoading(); },
  	            "fill never completed",
  	            4000
  	        );
  	        
  	        runs( function() { 	        	
-	 	    	companyCount = store.getCount();
-	 	        expect(companyCount).toBeGreaterThan(1);
+	 	        expect(companyStore.getCount()).toBeGreaterThan(1);
  	        });
  	    });
- 	    
- 	    it("should autoincrement company id during insert",function(){
- 	        companyCtrl.insertCompany();
- 	        company = store.getAt(store.getCount()-1);
+ 	     	    
+	    it("should persist new company record",function(){
+		    	var count = companyStore.getCount();
+
+		    	companyCtrl.insertCompany();
  	        toolbarCtrl.sync();
  	        waitsFor(
- 	        	function(){ return !store.commitRequired; },
+ 	        		function(){ return !companyStore.commitRequired; },
  	            "sync with insert failed",
  	            10000
  	 	    );
  	        runs (function() { 	        	
- 	        	expect(company.getId()).toNotEqual(-1);
+ 	        		expect(companyStore.getCount() - count).toEqual(1);
  	        });
- 	    });
+ 	    }); 	    
  	    
- 	    it("should update company",function(){
- 	    	modifiedCompanyName = "Company " + Ext.create('Ext.data.UuidGenerator').generate();
+ 	    it("should persist change of the company name",function(){
+	 	    	var company = companyStore.getAt(companyStore.getCount()-1);
+	 	    	
+ 	    		modifiedCompanyName = "Company " + Ext.create('Ext.data.UuidGenerator').generate();
  	        company.set('companyName', modifiedCompanyName);
  	        toolbarCtrl.sync();
  	        waitsFor(
- 	        	function(){ return !store.commitRequired; },
+ 	        	function(){ return !companyStore.commitRequired; },
  	            "sync with update failed",
  	            10000
  	 	    );
  	        runs( function() { 	        	
- 	        	toolbarCtrl.fill();
+ 	        		toolbarCtrl.fill();
  	        });
  	        waitsFor(
- 	        	function(){ return !store.isLoading(); },
+ 	        		function(){ return !companyStore.isLoading(); },
  	            "fill after update never completed",
  	            4000
  	 	    );
  	        runs (function() { 	        	
- 	        	expect(company.get('companyName')).toEqual(modifiedCompanyName);
+ 	        		expect(company.get('companyName')).toEqual(modifiedCompanyName);
  	        });
- 	    });	    
+ 	    });	
 
- 	    it("should delete company",function(){
- 	    	var count = store.getCount(); 	    	
- 	        store.removeAt(store.find( 'companyName', modifiedCompanyName, 0, false, false, true ));
+ 	    it("should persist deletion of a company", function(){
+ 	    		var count = companyStore.getCount(); 
+ 	        companyStore.removeAt(count - 1); // the last company is the one that we modified
  	        toolbarCtrl.sync();
  	        waitsFor(
- 	        	function(){ return !store.commitRequired; },
+ 	        	function(){ return !companyStore.commitRequired; },
  	            "sync with update failed",
  	            4000
  	 	    );
  	        runs( function() { 	        	
  	        	toolbarCtrl.fill();
  	        });
- 	        waitsFor(
- 	        	function(){ return !store.isLoading(); },
+ 	        waitsFor(function(){ return !companyStore.isLoading(); },
  	            "fill after delete never completed",
  	            4000
  	 	    );
  	        runs (function() { 	        	
- 	        	expect(store.getCount()).toEqual(count-1);
+ 	        		expect(companyStore.getCount()).toEqual(count-1);
  	        });
  	    });	
-/* 	    
- 	   it("should insert via associated stores",function(){
- 		    var associateStore;
- 		    
- 		    company = store.getAt(0);
- 		    associateStore = company.getAssociates();
+ 	   
+ 	    it("should autoincrement company id during commit with new company",function(){
+ 	    		var newCompany;
+ 	    		
+ 	        companyCtrl.insertCompany();
+ 	        newCompany = companyStore.getAt(companyStore.getCount()-1);
+ 	        toolbarCtrl.sync();
  	        waitsFor(
- 	        	function(){ return !associateStore.isLoading(); },
- 	            "fill never completed",
+ 	        		function(){ return !companyStore.commitRequired; },
+ 	            "sync with insert failed",
+ 	            10000
+ 	 	    );
+ 	        runs (function() { 	        	
+ 	        		expect(newCompany.getId()).toNotEqual(-1);
+ 	        });
+ 	    });
+ 	    
+	    it("should default foreign key in the new associate to id of the parent company",function(){
+	    		companyCtrl.insertCompany();
+			toolbarCtrl.sync();
+			waitsFor(
+				function(){ return !companyStore.commitRequired; },
+				"sync with insert failed",
+				10000
+			);  
+			var company = companyStore.getAt(companyStore.getCount()-1);
+	    		var associateStore = company.getAssociates();
+ 	        waitsFor(
+ 	        	function(){return !associateStore.isLoading(); },
+ 	            "get associates never completed",
  	            4000
  	 	    );
- 	        runs (function (){
- 	        	
+ 	        runs (function() { 	        	
+ 	           var associate = associateStore.createModel({
+ 	                associateName: 'Vasiliy Lokhankin'
+ 	            });
+	 	        associate.setId(associateStore.getLocalIdentity());	
+	 	        associateStore.add(associate);
+	 	        	expect(company.getId()===associate.companyId);
  	        });
-	    });	
-*/	    
-/*
+ 	    });
+	    
+	    
+	    it("should autoincrement id in the new associate during new parent/new child commit",function(){
+    			companyCtrl.insertCompany();
+			var company = companyStore.getAt(companyStore.getCount()-1);
+	    		var associateStore = company.getAssociates();
+ 	        waitsFor(
+ 	        		function(){return !associateStore.isLoading(); },
+ 	            "get associates never completed",
+ 	            4000
+ 	 	    );
+ 	        runs (function() { 	        	
+ 	           var associate = associateStore.createModel({
+ 	                associateName: 'Vasiliy Lokhankin'
+ 	            });
+	 	        associate.setId(associateStore.getLocalIdentity());	
+	 	        associateStore.add(associate);
+	 	        toolbarCtrl.sync();
+	 	        waitsFor(
+	 	        		function(){ return !associateStore.commitRequired; },
+	 	        		"sync with insert failed",
+	 	        		10000
+	 	        ); 
+	 	        runs (function() { 	 	        	
+	 	        		expect(expect(associate.getId()).toNotEqual(-1));
+	 	        });
+ 	        });
+	    });
+	    /*
  	    it("should open the editor window", function(){
  	        var grid = Ext.ComponentQuery.query('userlist')[0];
 
