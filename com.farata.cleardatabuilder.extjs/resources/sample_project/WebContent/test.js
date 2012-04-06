6dccc32f81c8b18b930f8428368d5c6f905be283
vrasputnis <vrasputnis@9db0787f-aee9-4a4d-a695-80f0cfb65743>
2012-04-06 04:58:14 +0000

    git-svn-id: https://cleartoolkit.svn.sourceforge.net/svnroot/cleartoolkit/HTML5/trunk@1471 9db0787f-aee9-4a4d-a695-80f0cfb65743

---
+++ com.farata.cleardatabuilder.extjs/resources/sample_project/WebContent/test.js
@@ -0,0 +1,34 @@
+Ext.Loader.setConfig({
+    disableCaching: false,
+    enabled: true,
+    paths: {
+    	Clear: 'clear',
+    	Test: 'test',
+    	**{app.name}**:'app'	
+    }
+});
+
+Ext.syncRequire('**{app.name}**.init.InitDirect');
+
+var Application = null;
+
+Ext.onReady(function() {
+    Application = Ext.create('Ext.app.Application', {
+        name: '**{app.name}**',
+        requires: [
+           'Clear.override.ExtJSOverrider',
+           'Test.spec.AllSpecs'
+        ],
+        controllers: [
+          'ToolbarController', 
+          'CompanyListController', 
+          'AssociateController'
+        ],
+        launch: function() {
+            jasmine.getEnv().addReporter(new jasmine.TrivialReporter());
+            jasmine.getEnv().execute();
+        }
+    //,autoCreateViewport:true
+    });
+});
+
