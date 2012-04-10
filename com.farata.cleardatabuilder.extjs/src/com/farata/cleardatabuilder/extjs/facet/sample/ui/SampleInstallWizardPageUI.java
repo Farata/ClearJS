5ba9c6512836bcef1fc3ecf3e33408a4dd1abaac
vrasputnis <vrasputnis@9db0787f-aee9-4a4d-a695-80f0cfb65743>
2012-04-10 22:41:20 +0000

    git-svn-id: https://cleartoolkit.svn.sourceforge.net/svnroot/cleartoolkit/HTML5/trunk@1505 9db0787f-aee9-4a4d-a695-80f0cfb65743

--- com.farata.cleardatabuilder.extjs/src/com/farata/cleardatabuilder/extjs/facet/sample/ui/SampleInstallWizardPageUI.java
+++ com.farata.cleardatabuilder.extjs/src/com/farata/cleardatabuilder/extjs/facet/sample/ui/SampleInstallWizardPageUI.java
@@ -53,7 +53,7 @@ public class SampleInstallWizardPageUI {
 		sampleCombo = new Combo(parent, SWT.NONE);
 		sampleCombo.add("Hibernate persistence");
 		sampleCombo.add("MyBatis persistence");
-		sampleCombo.add("HashMap persistence");
+		sampleCombo.add("InProcess persistence");
 		sampleCombo.select(0);
 		sampleCombo.addSelectionListener(new SelectionListener() {
 
