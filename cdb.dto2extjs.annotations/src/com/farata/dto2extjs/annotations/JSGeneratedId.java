fdbdfe3d5bcb5ac558808aebec66958bb49828b1
vrasputnis <vrasputnis@9db0787f-aee9-4a4d-a695-80f0cfb65743>
2012-04-28 19:02:12 +0000

    git-svn-id: https://cleartoolkit.svn.sourceforge.net/svnroot/cleartoolkit/HTML5/trunk@1531 9db0787f-aee9-4a4d-a695-80f0cfb65743

---
+++ cdb.dto2extjs.annotations/src/com/farata/dto2extjs/annotations/JSGeneratedId.java
@@ -0,0 +1,11 @@
+package com.farata.dto2extjs.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.FIELD}) 
+public @interface JSGeneratedId {
+}
