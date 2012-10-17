e04335b4f814150e00add5a1f3aad2da2fc0b1ca
alex_yuzafovich <alex_yuzafovich@9db0787f-aee9-4a4d-a695-80f0cfb65743>
2012-10-17 17:26:04 +0000

    directjngine.2.2
    git-svn-id: https://cleartoolkit.svn.sourceforge.net/svnroot/cleartoolkit/HTML5/trunk@1575 9db0787f-aee9-4a4d-a695-80f0cfb65743

--- clear-extjs-runtime-djn/src/clear/djn/servlet/SpringDispatcher.java
+++ clear-extjs-runtime-djn/src/clear/djn/servlet/SpringDispatcher.java
@@ -3,7 +3,7 @@ package clear.djn.servlet;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.softwarementors.extjs.djn.api.RegisteredAction;
+import com.softwarementors.extjs.djn.api.RegisteredMethod;
 import com.softwarementors.extjs.djn.servlet.ssm.SsmDispatcher;
 
 /**
@@ -15,22 +15,17 @@ public class SpringDispatcher extends SsmDispatcher {
 	private final Map<Class<?>, Object> mapClassBeanName = new HashMap<Class<?>, Object>();
 
 	@Override
-	protected Object createActionInstance(RegisteredAction action) {
+	protected Object getInvokeInstanceForNonStaticMethod(RegisteredMethod method) throws Exception {
 		Object actionInstance = null;
-		try {
-			if (this.mapClassBeanName.containsKey(action.getActionClass())) {
-				actionInstance = this.mapClassBeanName.get(action.getActionClass());
-			} else {
-				actionInstance = SpringLoaderHelper.getBeanOfType(action.getActionClass());
-				if (actionInstance == null) {
-					actionInstance = action.getActionClass().newInstance();
-				}
-				this.mapClassBeanName.put(action.getActionClass(), actionInstance);
+		if (this.mapClassBeanName.containsKey(method.getActionClass())) {
+			actionInstance = this.mapClassBeanName.get(method.getActionClass());
+		} else {
+			actionInstance = SpringLoaderHelper.getBeanOfType(method.getActionClass());
+			if (actionInstance == null) {
+				actionInstance = method.getActionClass().newInstance();
 			}
-		} catch (Exception e) {
-			throw createUnableToCreateActionInstanceException(action, e);
+			this.mapClassBeanName.put(method.getActionClass(), actionInstance);
 		}
-
 		return actionInstance;
 	}
 }
