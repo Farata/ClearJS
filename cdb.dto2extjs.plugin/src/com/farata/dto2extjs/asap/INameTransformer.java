8a82f3312c33b38ea4a9e1ef4d40e1da7cb2a7cc
Viktor Gamov <gamussa@gmail.com>
2014-01-02 01:32:13 -0500

    slightly organized imports for legacy annotation processor

--- cdb.dto2extjs.plugin/src/com/farata/dto2extjs/asap/INameTransformer.java
+++ cdb.dto2extjs.plugin/src/com/farata/dto2extjs/asap/INameTransformer.java
@@ -1,11 +1,11 @@
 package com.farata.dto2extjs.asap;
 
 public interface INameTransformer {
-	public String transform(String originalName);
-	
-	final public static INameTransformer NOP = new INameTransformer() {
-		public String transform(final String originalName) {
-			return originalName;
-		}
-	}; 
+    public String transform(String originalName);
+
+    final public static INameTransformer NOP = new INameTransformer() {
+        public String transform(final String originalName) {
+            return originalName;
+        }
+    };
 }
